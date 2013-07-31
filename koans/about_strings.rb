3925284c7686dbbe08f2a134768d83ea61663a92
Luke Aiken <lukeaiken@gmail.com>
2013-07-31 09:09:22 -0600

    Gaining karma by learning about_strings
    * single-quoted strings
    
    Gaining karma by learning about_strings
    
    * escaped quotes
    
    Gaining karma by learning about_strings
    
    * flexible quoting

--- koans/about_strings.rb
+++ koans/about_strings.rb
@@ -18,21 +18,21 @@ class AboutStrings < Neo::Koan
 
   def test_use_double_quotes_to_create_strings_with_single_quotes
     string = "Don't"
-    assert_equal __, string
+    assert_equal "Don't", string
   end
 
   def test_use_backslash_for_those_hard_cases
     a = "He said, \"Don't\""
     b = 'He said, "Don\'t"'
-    assert_equal __, a == b
+    assert_equal true, a == b
   end
 
   def test_use_flexible_quoting_to_handle_really_hard_cases
     a = %(flexible quotes can handle both ' and " characters)
     b = %!flexible quotes can handle both ' and " characters!
     c = %{flexible quotes can handle both ' and " characters}
-    assert_equal __, a == b
-    assert_equal __, a == c
+    assert_equal true, a == b
+    assert_equal true, a == c
   end
 
   def test_flexible_quotes_can_handle_multiple_lines
